1f1d40182297c7e944d70651b102f69b500e22b3
ConnorDeaneUZ <connordeane@urbanzoo.io>
2022-04-20 19:59:36 +0100

    movies added, nav bar started

---
+++ components/Global/NavBar/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="nav-bar">
+    <div class="nav-bar-container">
+
+      <search-bar />
+
+    </div>
+  </section>
+</template>
+
+<script>
+import SearchBar from '../SearchBar/SearchBar.vue'
+
+export default {
+  name: 'NavBar',
+  
+  components: {
+    SearchBar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'assets/scss/_color-palette.scss';
+
+.nav-bar {
+  background-color: #0d0f19;
+  width: 400px;
+  height: 100vh;
+}
+</style>
